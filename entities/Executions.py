db93750674705b214d8202140ae7171b9899621b
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-12 09:41:14 -0500

    Agregada URL de flower a la entidad Executions.

--- entities/Executions.py
+++ entities/Executions.py
@@ -3,13 +3,14 @@ from entities.Execution import Execution
 
 class Executions():
 
-	def __init__(self, conn):
+	def __init__(self, conn, flower):
 		self.conn = conn
+		self.flower = flower
 		self.executions = []
 
 	def load_enqueued_executing(self):
 		dao_execution = DAOExecution(self.conn)
 		for each_execution in dao_execution.get_enqueued_executing():
-			execution = Execution(each_execution, conn=self.conn)
+			execution = Execution(each_execution, conn=self.conn, flower=self.flower)
 			execution.load_tasks()
 			self.executions.append(execution)
