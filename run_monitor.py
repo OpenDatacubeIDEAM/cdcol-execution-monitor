dd0e80aee3c82914ebf93892a85a0385ef0aa743
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-11 20:54:56 -0500

    Agregado archivo run_monitor.py para ejecutar el monitor de ejecuciones.

---
+++ run_monitor.py
@@ -0,0 +1,44 @@
+# Datacube Ingestion Scheduler
+
+from ConfigParser import ConfigParser
+from entities.LockFile import LockFile
+from entities.Connection import Connection
+from entities.Executions import Executions
+from exceptions import Exception
+import os, sys, datetime, glob
+
+CONF_FILE = 'settings.conf'
+
+conf = ConfigParser()
+conf.read(CONF_FILE)
+
+lockfile = LockFile(conf.get('other','lock_file'))
+if lockfile.search():
+	print 'There\'s an execution in progress'
+	sys.exit(1)
+else:
+	lockfile.write()
+
+try:
+	print 'DATACUBE EXECUTION MONITOR'
+
+	dbconn = Connection(
+				host=conf.get('database','host'),
+				port=conf.get('database','port'),
+				name=conf.get('database','name'),
+				user=conf.get('database','user'),
+				password=conf.get('database','password')
+				)
+
+	dbconn.connect()
+
+	executions = Executions(dbconn.curr_conn)
+	executions.load_enqueued_executing()
+
+	for each_execution in executions.executions:
+		print str(each_execution._id) + '. ' + each_execution.description
+
+except Exception as e:
+	print 'Error: ' + str(e)
+finally:
+	lockfile.delete()
