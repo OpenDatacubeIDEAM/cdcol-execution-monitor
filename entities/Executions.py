b430a22f63396c0b78d632263b249b15488198ba
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-11 23:08:02 -0500

    Removida variable executions como variable estática. Se cargan además la lista de tareas por cada ejecución.

--- entities/Executions.py
+++ entities/Executions.py
@@ -3,13 +3,13 @@ from entities.Execution import Execution
 
 class Executions():
 
-	executions = []
-
 	def __init__(self, conn):
 		self.conn = conn
+		self.executions = []
 
 	def load_enqueued_executing(self):
 		dao_execution = DAOExecution(self.conn)
 		for each_execution in dao_execution.get_enqueued_executing():
 			execution = Execution(each_execution, conn=self.conn)
+			execution.load_tasks()
 			self.executions.append(execution)
