108db30ad5dfcc27ac2758ee8de2aecae40b195e
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-12 09:38:51 -0500

    Agregada URL de flower a la entidad Tasks y llamado al método sync de Task.

--- entities/Tasks.py
+++ entities/Tasks.py
@@ -3,12 +3,14 @@ from entities.Task import Task
 
 class Tasks():
 
-	def __init__(self, conn):
+	def __init__(self, conn, flower):
 		self.conn = conn
+		self.flower = flower
 		self.tasks = []
 
 	def load_by_exec_id(self, exec_id):
 		dao_task = DAOTask(self.conn)
 		for each_task in dao_task.get_by_exec_id(exec_id):
-			task = Task(each_task, conn=self.conn)
+			task = Task(each_task, conn=self.conn, flower=self.flower)
+			task.sync()
 			self.tasks.append(task)
