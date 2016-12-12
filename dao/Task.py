5dd9eb6cb1587342a4d184662b965a0fea3b94cb
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-12 10:16:54 -0500

    Agregado método update en el DAO Task.

--- dao/Task.py
+++ dao/Task.py
@@ -22,3 +22,18 @@ class Task():
 					'WHERE execution_id = \'' + str(exec_id) + '\';')
 		rows = cur.fetchall()
 		return rows
+
+	def update(self, _id, start_date, end_date, state, state_updated_at, updated_at):
+		cur = self.conn.cursor(cursor_factory=DictCursor)
+		query = ('UPDATE execution_task SET ' +
+				'start_date=\'' + str(start_date) + '\', ' +
+				'end_date=\'' + str(end_date) + '\', ' +
+				'state=\'' + str(state) + '\', ' +
+				'state_updated_at=\'' + str(state_updated_at) + '\', ' +
+				'updated_at=\'' + str(updated_at) + '\' ' +
+				'WHERE id=' + str(_id) + ';')
+
+		query = query.replace('\'None\'', 'NULL')
+		cur.execute(query)
+		self.conn.commit()
+		cur.close()
