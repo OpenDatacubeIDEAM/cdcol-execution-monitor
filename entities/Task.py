f192b399d3d59d4c6fc0089cf8a56a69ddacef74
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-12 09:37:40 -0500

    Agregada URL de FLower a la entidad Task y método sync para actualizar estados

--- entities/Task.py
+++ entities/Task.py
@@ -1,21 +1,27 @@
 from dao.Task import Task as DAOTask
-import datetime
+from urllib2 import urlopen
+import datetime, json
 
 class Task():
 
 	STATES= {
-			'PENDING_STATE':'1',
-			'RECEIVED_STATE':'2',
-			'STARTED_STATE':'3',
-			'SUCCESS_STATE':'4',
-			'FAILURE_STATE':'5',
-			'REVOKED_STATE':'6',
-			'RETRY_STATE':'7'
+			'PENDING':'1',
+			'RECEIVED':'2',
+			'STARTED':'3',
+			'SUCCESS':'4',
+			'FAILURE':'5',
+			'REVOKED':'6',
+			'RETRY':'7'
 			}
 
-	def __init__(self, dao_task, conn=None):
+	def get_date(self, timestamp_str):
+
+		return str(datetime.date.fromtimestamp(float(timestamp_str)))
+
+	def __init__(self, dao_task, conn=None, flower=None):
 
 		self.conn = conn
+		self.flower = flower
 		self._id = dao_task['id']
 		self.uuid = dao_task['uuid']
 		self.start_date = dao_task['start_date']
@@ -25,3 +31,20 @@ class Task():
 		self.created_at = dao_task['created_at']
 		self.updated_at = dao_task['updated_at']
 		self.execution_id = dao_task['execution_id']
+
+	def sync(self):
+
+		task = json.loads(urlopen(self.flower + '/api/tasks').read())[self.uuid]
+
+		if self.state != self.STATES[task['state']]:
+			self.state = self.STATES[task['state']]
+			if self.state != self.STATES['PENDING'] or self.state != self.STATE['RECEIVED']:
+				self.start_date = self.get_date(task['started'])
+			if self.state == self.STATES['SUCCESS']:
+				self.end_date = self.get_date(task['succeeded'])
+			elif self.state == self.STATES['FAILURE']:
+				self.end_date = self.get_date(task['failed'])
+			elif self.state == self.STATES['REVOKED']:
+				self.end_date = self.get_date(task['revoked'])
+			self.state_updated_at = str(datetime.datetime.now())
+			self.updated_at = str(datetime.datetime.now())
