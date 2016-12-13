10f73aa975315db50c81345e88994082747a901e
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-13 09:39:29 -0500

    Agregado método para limpiar el campo trace_error. Se reemplazan los ' por " para evitar errores con postgresql.

--- dao/Execution.py
+++ dao/Execution.py
@@ -31,8 +31,18 @@ class Execution():
 				'state= \'' + str(state) + '\', ' +
 				'started_at= \'' + str(started_at) + '\', ' +
 				'finished_at= \'' + str(finished_at) + '\', ' +
-				'trace_error= \'' + str(trace_error) + '\', ' +
+				'trace_error= \'' + str(trace_error).replace('\'', '"') + '\', ' +
 				'updated_at= \'' + str(updated_at) + '\' ' +
+				'WHERE id=' + str(_id) + ';')
+		query = query.replace('\'None\'', 'NULL')
+		cur.execute(query)
+		self.conn.commit()
+		cur.close()
+
+	def flush_trace_error(self, _id):
+		cur = self.conn.cursor(cursor_factory=DictCursor)
+		query = ('UPDATE execution_execution SET ' +
+				'trace_error= \'\' ' +
 				'WHERE id=' + str(_id) + ';')
 		query = query.replace('\'None\'', 'NULL')
 		cur.execute(query)
