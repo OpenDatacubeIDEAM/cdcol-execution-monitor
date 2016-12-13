df56a88f6bedfbf70a5578f5125b447bb70217ad
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-13 09:41:48 -0500

    Agregado parámetro para retornar el traceback de todas las tareas de una ejecución.

--- entities/Tasks.py
+++ entities/Tasks.py
@@ -8,10 +8,10 @@ class Tasks():
 		self.flower = flower
 		self.tasks = []
 
-	def load_by_exec_id(self, exec_id):
+	def load_by_exec_id(self, exec_id, trace_error=None):
 		dao_task = DAOTask(self.conn)
 		for each_task in dao_task.get_by_exec_id(exec_id):
 			task = Task(each_task, conn=self.conn, flower=self.flower)
-			task.sync()
+			task.sync(trace_error=trace_error)
 			task.save()
 			self.tasks.append(task)
