d3af5f76976c8a7b54002d5c20d9e4629e2bc6bb
Juan Pablo Vinchira Salazar <cronot99@gmail.com>
2016-12-14 12:15:59 -0500

    Actualizado archivo run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+EXEC_MON_FOLDER=$HOME/execution-monitor
+PYTHON=$HOME/anaconda2/bin/python
+
 echo "$(date) RUNNING EXECUTION MONITOR"
 source $HOME/.bashrc
-cd $HOME/execution-monitor
-$HOME/anaconda2/bin/python run_monitor.py
+cd $EXEC_MON_FOLDER
+$PYTHON run_monitor.py
 echo "$(date) DONE"
